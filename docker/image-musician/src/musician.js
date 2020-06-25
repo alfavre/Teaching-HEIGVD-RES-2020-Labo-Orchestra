7e812c93cf3258fa05230c0b3010f0017b0ff7b9
Alban <you@example.com>
2020-06-25 17:32:58 +0200

    First musician draft and some files changes

---
+++ docker/image-musician/src/musician.js
@@ -0,0 +1,68 @@
+// things for udp stolen from thermometer
+
+// this next line seems important we should look into it
+/*
+const protocol = require('./musician-protovol');
+*/
+
+// the "imports"
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+// the constants
+const TIME=1000;
+const PROTOCOL_PORT=;
+const PROTOCOL_MULTICAST_ADDRESS;
+
+
+// we should create a map with sound-instrument.
+const instrumentSoundMap = new Map();
+instrumentSoundMap.set('piano', 'ti-ta-ti');
+instrumentSoundMap.set('trumpet', 'pouet');
+instrumentSoundMap.set('flute', 'trulu');
+instrumentSoundMap.set('violin', 'gzi-gzi');
+instrumentSoundMap.set('drum', 'boum-boum');
+
+
+
+// do some parameter control, we only want the basic info and the instrument type
+
+if (process.argv.length != 3) {
+    console.error('Only one argument required');
+    return;
+}
+
+//we set the instrument
+const instrument = process.argv[2];
+
+if (!instrumentSoundMap.has(instument)) {
+    console.error('Illegal instrument! we called the music police');
+    return;
+}
+
+//we set the musician (it's a JSON Object)
+const musician = musicianPLays(instrument);
+const strMusician = JSON.stringify(musician);
+
+//play a lot
+server.on("listening", () => {
+    setInterval(playOnce, TIME);
+});
+
+
+function playOnce(){
+    server.send(strMusician, 0, strMusician.length,PROTOCOL_PORT,PROTOCOL_MULTICAST_ADDRESS);
+    console.log("sending the sweet power of jazz");
+}
+
+// just a function unlike thermometer, returns an anonymous object (maybe do a musician class?)
+function musicianPLays(instrumentPlayed) {
+    return {
+        uuid: 'placeholder',
+        sound: instrumentSoundMap.get(instrumentPlayed),
+        instrument: instrumentPlayed,
+        timeCreated: 'PLACEHOLEDER'
+    }
+}
+
+

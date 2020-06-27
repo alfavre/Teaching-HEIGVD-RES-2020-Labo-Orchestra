812494672b013011352ce208391d866b39fda887
Alban <you@example.com>
2020-06-27 17:14:07 +0200

    lots of fixes

--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -4,18 +4,24 @@
 /*
 const protocol = require('./musician-protovol');
 */
+console.log('salut c cool');
 
 // the "imports"
 const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
 const moment = require('moment');
-const uuid = require('uuid');
+
+console.log('salut c superseuper');
+
+const { v4: uuidv4 } = require('uuid');
+
+console.log('salut c 2');
 
 // the constants
 const TIME=1000;
 const PROTOCOL_PORT=2020;
 const PROTOCOL_MULTICAST_ADDRESS='239.255.22.5'; // address stolen from thermometer
 
+console.log('salut c 3');
 
 // we should create a map with sound-instrument.
 const instrumentSoundMap = new Map();
@@ -25,19 +31,21 @@ instrumentSoundMap.set('flute', 'trulu');
 instrumentSoundMap.set('violin', 'gzi-gzi');
 instrumentSoundMap.set('drum', 'boum-boum');
 
-
+const server = dgram.createSocket('udp4');
 
 // do some parameter control, we only want the basic info and the instrument type
+
+//we set the instrument
+const instrument = process.argv[2];
 
 if (process.argv.length != 3) {
     console.error('Only one argument required');
     return;
 }
 
-//we set the instrument
-const instrument = process.argv[2];
+console.log(instrument);
 
-if (!instrumentSoundMap.has(instument)) {
+if (!instrumentSoundMap.has(instrument)) {
     console.error('Illegal instrument! we called the music police');
     return;
 }
@@ -49,7 +57,7 @@ const strMusician = JSON.stringify(musician);
 
 //play a lot
 server.on("listening", () => {
-    setInterval(playOnce, TIME);
+    setInterval(playOnce(), TIME);
 });
 
 
@@ -61,7 +69,7 @@ function playOnce(){
 // just a function unlike thermometer, returns an anonymous object (maybe do a musician class?)
 function musicianPLays(instrumentPlayed) {
     return {
-        uuid: uuid.v4(), // might be a problem as we are not in an object
+        uuid: uuidv4(), // might be a problem as we are not in an object
         sound: instrumentSoundMap.get(instrumentPlayed),
         instrument: instrumentPlayed,
         activeSince: moment().format(),
